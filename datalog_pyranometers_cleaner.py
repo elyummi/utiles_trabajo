e19d169e03649b6c6c2cb75b8981f1e43b184b76
elyummi <79262532+elyummi@users.noreply.github.com>
2021-07-28 13:51:53 +0200

    Add files via upload

--- datalog_pyranometers_cleaner.py
+++ datalog_pyranometers_cleaner.py
@@ -5,8 +5,8 @@
 
 #Variables
 #Valores en mV. Equivalente en mV de la irradiancia(600W/m2 <-> 7-8 mV)
-irradiancia_min_defecto = 7
-irradiancia_min = 11
+irradiancia_min_defecto = 6
+irradiancia_min = 7
 #Columna (entrada del registrador), de la que queremos los datos (Entre 1 y 9). Columna 0 es la primera
 columna_deseada = 1
 
@@ -25,28 +25,49 @@ def limpiar_df(df_full,columna_deseada,irradiancia_min = irradiancia_min_defecto
     y devuelve la informacion como otro csv
     '''
     print("Limpiando el archivo.")
+    print("Eliminando valores menores de ", irradiancia_min, "mV.")
     #Recoge el csv importado, formatea los valores a int y elimina a en funcion de la irradiancia minima
 
     #Elimina las columnas que no necesitamos.
-    df_full = df_full.drop(columns=["A1234567890"])
-    df_full = df_full.drop(columns=["AO1234"])
+    try:
+        df_full = df_full.drop(columns=["A1234567890"])
+    except:
+        pass
+    try:
+        df_full = df_full.drop(columns=["AO1234"])
+    except:
+        pass
     for i in range(1,10):
         #Pandas llama: nombre + .num, a la columnas con nombres repetidos
         if i is not columna_deseada:
             nombre_columna_eliminar = "mV."+str(i)
-            df_full = df_full.drop(columns = [nombre_columna_eliminar])
+            try:
+                df_full = df_full.drop(columns = [nombre_columna_eliminar])
+            except:
+                pass
     # Convierte los valores del csv en bruto del formato "+- valor" a int, para que python
     # pueda trabajar con ellos
     for index in range(len(df_full)):
-        df_full.at[index, "mV"] = pd.to_numeric(df_full.loc[df_full.index[index], "mV"].strip(" +-"))
+        try:
+            df_full.at[index, "mV"] = pd.to_numeric(df_full.loc[df_full.index[index], "mV"].strip(" +-"))
+        except:
+            #Si el numero esta delimitado por ",". conevertimos la "," en "." y probamos de nuevo
+            #Esto puede pasar si el archivo csv no esta delimitado por ","
+            df_full.at[index, "mV"] = pd.to_numeric(df_full.loc[df_full.index[index], "mV"].strip(" +-").replace(",","."))
     #Lo mismo pero para la columna deseada
     nombre_columna_deseada = "mV." + str(columna_deseada)
     for index in range(len(df_full)):
-        df_full.at[index, nombre_columna_deseada] = pd.to_numeric(df_full.loc[df_full.index[index], nombre_columna_deseada].strip(" +-"))
+        try:
+            df_full.at[index, nombre_columna_deseada] = pd.to_numeric(df_full.loc[df_full.index[index], nombre_columna_deseada].strip(" +-"))
+        except:
+            df_full.at[index, nombre_columna_deseada] = pd.to_numeric(df_full.loc[df_full.index[index], nombre_columna_deseada].strip(" +-").replace(",", "."))
     # Elimina los valores que son menores al valor irradiancia.min, especificado al inicio
     df_clean = df_full.drop(df_full[df_full.mV < irradiancia_min].index)
+    #Da nuevos indices la primera columna, empezando desde 1
+    for i in range(df_clean.shape[0]):
+        df_clean.iloc[i,0] = i+1
     # Pasamos el DF limpio a csv. Salida a donde se ha ejecutado el programa
-    df_clean.to_csv("limpio.csv", index=False)
+    df_clean.to_csv("limpio_2.csv", index=False)
     #Falta indicar aqui la ruta de salida del archivo
     print("Nuevo archivo listo. Ruta: ")
     return df_clean
@@ -61,16 +82,9 @@ def limpiar_df(df_full,columna_deseada,irradiancia_min = irradiancia_min_defecto
 #Parte 1: abrir y limpiar el csv
 
 #Path donde se encuentra el archivo a limpiar
-path_csv_bruto = r"C:\Users\daniel.villoslada\PycharmProjects\pythonProject\datalog_bruto.CSV"
+path_csv_bruto = r"C:\Users\daniel.villoslada\PycharmProjects\pythonProject\datalog_bruto_2.csv"
 
 #Crea un DF de pandas sin tener en cuenta las 30 primeras filas, ya que no contienen los datos que queremos
-df_full = pd.read_csv(path_csv_bruto,skiprows = 30) #Dataframe de Pandas. Las 30 primeras filas dan problemas
+df_full = pd.read_csv(path_csv_bruto,skiprows = 30, sep = ";") #Dataframe de Pandas. Las 30 primeras filas dan problemas
 #Limpiamos el csv
 df_clean = limpiar_df(df_full,columna_deseada,irradiancia_min)
-#--------------------------------------------------------------------------------
-#Parte 2: analisis de los datos
-
-
-
-#---------------------------------------------------------------------------------
-#Parte 3: salida de resultado y grafico
